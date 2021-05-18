93b01068e5ccb82b222f858d54fbeece87bced6d
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-05-18 23:03:03 +0600

    Check prime optimal approach add

--- Number Theory/prime_number.cpp
+++ Number Theory/prime_number.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool optimalPrime(int n){
+	if(n<1) return false;
+	for(int i=2;i*i<n;i++){
+		if(n%i==0) return false;
+	}
+	return true;
+}
+
 bool primeNumber(int n){
 	if(n<1) return false;
 	for(int i=2;i<n;i++){
@@ -15,9 +23,13 @@ int main(){
 	cin>>n;
 	if(primeNumber(n)) cout<< "Prime number!!!\n";
 	else cout<< "Not prime number!!!\n";
+	//Time complexity O(n)
+
+	if(optimalPrime(n)) cout<< "Prime number!!!\n";
+	else cout<< "Not prime number!!!\n";
+	//Time complexity O(/n)
 
 	return 0;
 }
 
-//Time complexity O(n)
 
