50590b4b83962a53dfa68d3ccd422b47be79edf9
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-05-21 23:58:26 +0600

    Binary euclid algorithm

--- Number Theory/euclids.cpp
+++ Number Theory/euclids.cpp
@@ -27,3 +27,34 @@ int gcd(int a, int b)
    
     return gcd(b%a, a);
 }
+
+
+// Approach 1(Optimization):Binary Euclid Algorithm
+// Complexity:
+// Time:O(Log(a+b)), O(Log(n))
+// Space:O(1)
+// Efficient C++ program when % and / are not allowed
+int gcd(int a, int b)
+{
+    // Base cases
+    if (b == 0 || a == b) return a;
+    if (a == 0) return b;
+
+    // If both a and b are even, divide both a
+    // and b by 2. And multiply the result with 2
+    if ( (a & 1) == 0 && (b & 1) == 0 )
+    return gcd(a>>1, b>>1) << 1;
+
+    // If a is even and b is odd, divide a by 2
+    if ( (a & 1) == 0 && (b & 1) != 0 )
+    return gcd(a>>1, b);
+
+    // If a is odd and b is even, divide b by 2
+    if ( (a & 1) != 0 && (b & 1) == 0 )
+    return gcd(a, b>>1);
+
+    // If both are odd, then apply normal subtraction
+    // algorithm. Note that odd-odd case always
+    // converts odd-even case after one recursion
+    return (a > b)? gcd(a-b, b): gcd(a, b-a);
+}
