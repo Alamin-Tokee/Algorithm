767560e03b0e57461fa9085d9d372391b837f5d0
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-05-21 23:56:24 +0600

    Another approach euclids algorithm

--- Number Theory/euclids.cpp
+++ Number Theory/euclids.cpp
@@ -1,7 +1,7 @@
-Approach 1 (Using subtraction)
-Complexity:
-Time:O(n) worst case
-Space:O(1)
+// Approach 1 (Using subtraction)
+// Complexity:
+// Time:O(n) worst case
+// Space:O(1)
 
 // Recursive function to return gcd of a and b
 int gcd(int a, int b)
@@ -11,3 +11,19 @@ int gcd(int a, int b)
    
     return (a > b)? gcd(a-b, b): gcd(a, b-a);
 } 
+
+
+
+
+// Approach 2(Using modulo operator)
+// Complexity:
+// Time:O(Log min(a, b))
+// Space:O(1)
+// Function to return gcd of a and b
+int gcd(int a, int b)
+{
+    if (a == 0)
+    return b;
+   
+    return gcd(b%a, a);
+}
