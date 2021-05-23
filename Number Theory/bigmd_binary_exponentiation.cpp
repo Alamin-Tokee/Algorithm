f9acd8addc518085266056e9e399269b984f35f6
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-05-23 23:52:51 +0600

    Bigmod iterative using binary exponentiation

--- Number Theory/bigmd_binary_exponentiation.cpp
+++ Number Theory/bigmd_binary_exponentiation.cpp
@@ -2,13 +2,24 @@
 
 long bigmodBinaryExponentiationRecursive(long a,long b,int m){
 
-	if(b == 0) return 1 % M; 
+	if(b == 0) return 1 % m; 
 
 	long ans=binaryExponentiationRecursive(a,b/2,m);
 
 	ans = ans * ans % m;
 	if(b % 2) ans = ans * a % m;
 
+	return ans;
+}
+long bigmodBinaryExponentiationIterative(long a,long b,long m){
+	a = a % m;
+	long ans = 1;
+	while(b > 0){
+		if(b & 1)
+			ans=ans * a % m;
+		a = a * a % m;
+		b >>= 1;
+	}
 	return ans;
 }
 
@@ -18,7 +29,9 @@ int main(){
 	std::cin>>a>>b>>m;
 
 	long result1=bigmodBinaryExponentiationRecursive(long a,long b,long m);
-	std::cout<< result1 << '\n';
+	long result2=bigmodBinaryExponentiationIterative(long a,long b,long m);
+
+	std::cout<< result1 << " " << result2 << '\n';
 
 	return 0;
 }
