d8b73505850176bb80f8fd7b39fd337ab2118168
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-05-23 23:38:26 +0600

    Binary Exponentiation Iterative

--- Number Theory/binary_exponentiation.cpp
+++ Number Theory/binary_exponentiation.cpp
@@ -15,12 +15,27 @@ long binaryExponentiationRecursive(long a,long b){
 	return ans;
 }
 
+long binaryExponentiationIterative(long a,long b){
+
+	long ans=1;
+	while(b>0){
+		if(b & 1)
+			ans=ans*ans*a;
+		a=a*a;
+		b>>=1;
+	}
+	return ans;
+}
+
 int main(){
 	long a,b;
 
 	std::cin>>a>>b;
 
-	long result=binaryExponentiationRecursive(long a,long b);
+	long result1=binaryExponentiationRecursive(long a,long b);
+	long result2=binaryExponentiationIterative(long a,long b);
 
-	std::cout<< result << '\n';
+	std::cout<< result1 << " " << result2 << '\n';
+
+	return 0;
 }
