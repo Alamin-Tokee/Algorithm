ef2a0729acf5fad3272250ddee84e4238b4f4cc1
Al-Amin Tokee <61968457+Alamin-Tokee@users.noreply.github.com>
2021-10-07 00:06:18 +0600

    Dynamic Programming min coin change problem

--- Dynamic Programing/min_coin_change.cpp
+++ Dynamic Programing/min_coin_change.cpp
@@ -83,3 +83,23 @@ public:
         return minCoins == INT_MAX - 1 ? -1 : minCoins;    
     }
 };
+
+
+//DP Tabulization(1D)
+
+class Solution{
+public:
+    int coinChange(vector& coins, int amount) {
+        int Max = amount + 1;
+        vector dp(amount + 1, Max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.size(); j++) {
+                if (coins[j] <= i) {
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+};
